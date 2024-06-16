ef52d4507706d7d55b9335cc9ec871d47fa9a6f3
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-06-16 06:09:53 +0200

    get and post routes

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const Book = require('./models/book');
+
+mongoose.connect('mongodb+srv://rampriyakarthick:0xRY9RCXk8trgylI@cluster0.uwfvjpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.get('/api/books', (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+  });
+
+//authentication requis
+app.post('/api/books', (req, res, next) => {
+    delete req.body._id;
+    const book = new Book({
+      ...req.body
+    });
+    Book.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.get('/api/books/:id ', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(book))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+module.exports = app;
