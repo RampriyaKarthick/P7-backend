0ed4eb2392d6ff8130504bc4920ecf5384c3f6a9
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-06-16 07:04:00 +0200

    controllers folder

---
+++ controllers/book.js
@@ -0,0 +1,48 @@
+const Book = require('../models/book')
+
+exports.createBook = (req, res, next) => {
+   
+    const book = new Book({
+        userId: req.body.userId,
+        title:req.body.title,
+        author : req.body.author,
+        imageUrl : req.body.imageUrl,
+        year: req.body.year,
+        genre:req.body.genre,
+        ratings : [
+        {
+        userId: req.body.userId,
+        grade :req.body.grade,
+        }
+        ],
+        averageRating : req.body.averageRating,
+        }
+        ) 
+    Book.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch((error) => res.status(400).json({ error :error}));
+  };
+
+  exports.getAllBooks = (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+  }
+
+  exports.getSingleBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(book))
+      .catch(error => res.status(404).json({ error }));
+  }
+
+  exports.updateEachBook =(req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+       .catch(error => res.status(400).json({ error }));
+   }
+
+   exports.deleteBook = (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  }
--- routes/book.js
+++ routes/book.js
@@ -2,40 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 const Book = require('../models/book')
+const bookCtrl = require('../controllers/book');
 
-router.get('/', (req, res, next) => {
-    Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-  });
+router.get('/', bookCtrl.getAllBooks);
 
 //authentication requis
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body
-    });
-    Book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.post('/',  bookCtrl.createBook);
 
- router.get('/:id ', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  });
+ router.get('/:id ', bookCtrl.getSingleBook);
 
- router.put('/:id', (req, res, next) => {
-   Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+ router.put('/:id', bookCtrl.updateEachBook);
 
-  router.delete('/:id ', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+  router.delete('/:id ',bookCtrl.deleteBook );
 
 module.exports = router;
