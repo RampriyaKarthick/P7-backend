79cef1a6d72a72fb9c2ffe06d970ab337f8b8709
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-06-16 06:18:19 +0200

    edit and delete routes for books

--- app.js
+++ app.js
@@ -42,4 +42,16 @@ app.post('/api/books', (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   });
 
+  app.put('/api/books/:id', (req, res, next) => {
+   Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.delete('/api/books/:id ', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
 module.exports = app;
